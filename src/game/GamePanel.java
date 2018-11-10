df6351ced71fe1fc0e12bc571d22f27f6c46388e
alessanderleite <alessander.logistica@gmail.com>
2018-11-10 16:30:52 -0200

    initial commit

---
+++ src/game/GamePanel.java
@@ -0,0 +1,31 @@
+package game;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class GamePanel extends JPanel implements Runnable, KeyListener {
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+
+	}
+
+	@Override
+	public void run() {
+
+	}
+
+}
